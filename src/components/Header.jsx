2d508a1926693b25b2aceb72d2641798877147f8
Moises Roberto Sanchez Perez <moisesrobertosanchezperez@gmail.com>
2024-03-30 16:11:35 -0600

    Añadiendo git-icon-social

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,10 @@ import './Header.css'
 export default function Header({ title }) {
   return (
     <header className="header">
-      <h1>{ title }</h1>
+      <h1>
+      { title }
+      </h1>
+      <a href="https://github.com/moisesRmx" target="_blank"><img src="public/github-icon.png" alt="github icon" /></a>
     </header>
   )
 }
